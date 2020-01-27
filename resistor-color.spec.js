89c43570f0806d6c095645fc517f3270eac67061
Austin <austin@8thlight.com>
2020-01-27 15:56:01 -0800

    add second test

--- resistor-color.spec.js
+++ resistor-color.spec.js
@@ -4,4 +4,8 @@ describe("colorCode", () => {
   test("it returns 'Black' when given the number 0.", () => {
     expect(colorCode(0)).toEqual("Black")
   })
+
+  test("it returns 'Brown' when given the number 1.", () => {
+    expect(colorCode(1)).toEqual("Brown")
+  })
 })
