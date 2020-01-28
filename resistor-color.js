58eff6df21e85bb0ac15b26798546126a3ecc4f0
Austin <austin@8thlight.com>
2020-01-27 16:55:45 -0800

    alter tests and functionality to properly reflect expectations of the code being returned given the color rather than the reverse. Also used give, act, expect pattern to write the tests on this refactor

--- resistor-color.js
+++ resistor-color.js
@@ -3,8 +3,8 @@
 // convenience to get you started writing code faster.
 //
 
-export const colorCode = (code) => {
-  return COLORS[code]
+export const colorCode = (color) => {
+  return COLORS.indexOf(color)
 };
 
-export const COLORS = ["Black", "Brown", "Red", "Orange", "Yellow", "Green", "Blue", "Violet", "Grey", "White"];
+export const COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"];
--- resistor-color.spec.js
+++ resistor-color.spec.js
@@ -1,43 +1,63 @@
 import { colorCode } from './resistor-color.js'
 
 describe("colorCode", () => {
-  test("it returns 'Black' when given the number 0.", () => {
-    expect(colorCode(0)).toEqual("Black")
+  test("it returns 0 when given the color 'black'.", () => {
+    const given = "black"
+    const action = colorCode(given)
+    expect(action).toEqual(0)
   })
 
-  test("it returns 'Brown' when given the number 1.", () => {
-    expect(colorCode(1)).toEqual("Brown")
+  test("it returns 1 when given the color 'brown'.", () => {
+    const given = "brown"
+    const action = colorCode(given)
+    expect(action).toEqual(1)
   })
 
-  test("it returns 'Red' when given the number 2.", () => {
-    expect(colorCode(2)).toEqual('Red')
+  test("it returns 2 when given the color 'red'.", () => {
+    const given = "red"
+    const action = colorCode(given)
+    expect(action).toEqual(2)
   })
 
-  test("it returns 'Orange' when given the number 3.", () => {
-    expect(colorCode(3)).toEqual('Orange')
+  test("it returns 3 when given the color 'orange'.", () => {
+    const given = "orange"
+    const action = colorCode(given)
+    expect(action).toEqual(3)
   })
 
-  test("it returns 'Yellow' when given the number 4.", () => {
-    expect(colorCode(4)).toEqual('Yellow')
+  test("it returns 4 when given the color 'yellow'.", () => {
+    const given = "yellow"
+    const action = colorCode(given)
+    expect(action).toEqual(4)
   })
 
-  test("it returns 'Green' when given the number 5.", () => {
-    expect(colorCode(5)).toEqual('Green')
+  test("it returns 5 when given the color 'green'.", () => {
+    const given = "green"
+    const action = colorCode(given)
+    expect(action).toEqual(5)
   })
 
-  test("it returns 'Blue' when given the number 6.", () => {
-    expect(colorCode(6)).toEqual('Blue')
+  test("it returns 6 when given the color 'blue'.", () => {
+    const given = "blue"
+    const action = colorCode(given)
+    expect(action).toEqual(6)
   })
 
-  test("it returns 'Violet' when given the number 7.", () => {
-    expect(colorCode(7)).toEqual('Violet')
+  test("it returns 7 when given the color 'violet'.", () => {
+    const given = "violet"
+    const action = colorCode(given)
+    expect(action).toEqual(7)
   })
 
-  test("it returns 'Grey' when given the number 8.", () => {
-    expect(colorCode(8)).toEqual('Grey')
+  test("it returns 8 when given the color 'grey'.", () => {
+    const given = "grey"
+    const action = colorCode(given)
+    expect(action).toEqual(8)
   })
 
-  test("it returns 'White' when given the number 9.", () => {
-    expect(colorCode(9)).toEqual('White')
+  test("it returns 9 when given the color 'white'.", () => {
+    const given = "white"
+    const action = colorCode(given)
+    expect(action).toEqual(9)
   })
 })
